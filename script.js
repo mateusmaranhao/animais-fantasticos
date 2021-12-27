fd3d2d4b3fe118fd75d3a02d74c1fd0fd062dd24
Mateus <mateusmaranhao0210@gmail.com>
2021-12-27 07:36:25 -0300

    criar animação ao scroll

--- script.js
+++ script.js
@@ -51,13 +51,33 @@ function initSmoothScroll() {
     const section = document.querySelector(href);
 
     section.scrollIntoView({
-      block: 'start',
       behavior: 'smooth',
+      block: 'start',
     });
-  };
+  }
 
-  linksInternos.forEach((item) => {
-    item.addEventListener('click', scrollToSection);
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
   });
 };
 initSmoothScroll();
+
+function initScrollAnimation() {
+  const sections = document.querySelectorAll('.js-scroll');
+  if(sections.length){
+    const halfWindow = window.innerHeight * 0.6;
+
+    function animaScroll(){
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - halfWindow) < 0;
+        if(isSectionVisible) {
+          section.classList.add('ativo');
+        }
+      })
+    }
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initScrollAnimation();
